79404fe2285e0a3262a9186d63feaf31e055d845
Ahmed Hassop <Ahmed Hassob>
2024-12-01 20:50:18 +0200

    fix error in orders page

--- app/(pages)/vendor/orders/page.jsx
+++ app/(pages)/vendor/orders/page.jsx
@@ -5,6 +5,7 @@ import Order from "@/app/_components/Order";
 import { vendorContext } from "@/app/_context/vendorManagement";
 
 export default function Orders() {
-  const { recentOrder } = useContext(vendorContext);
-  return <Order recentOrder={recentOrder} />;
+  // const { recentOrder } = useContext(vendorContext);
+  // return <Order recentOrder={recentOrder} />;
+  return <></>;
 }
