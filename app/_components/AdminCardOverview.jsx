cc320e79461b839086d98ab693dfd045986f0f90
Ahmed Hassop <Ahmed Hassob>
2024-12-13 16:57:12 +0200

    improve some performance\

--- app/_components/AdminCardOverview.jsx
+++ app/_components/AdminCardOverview.jsx
@@ -12,7 +12,7 @@ export default function AdminCardOverview({ cardOverviewData }) {
         animate={{ scale: 1, y: 0 }}
         transition={{ duration: 1, delay: index * 0.2 }}>
         <div className="body">
-          <h3 className="title">{card.title}</h3>
+          <h4 className="title">{card.title}</h4>
           <strong className="amount">{card.amount}</strong>
           <div className="details">
             <span className="increase">{card.increase}</span>
